2f97ba63c08f2fd51c4b714ef36f094e7be642af
Samson OCHE <s.oche@outlook.com>
2025-03-21 18:39:54 +0100

    added DB password environ

--- django_project/settings.py
+++ django_project/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD_PG'),
+        'PASSWORD': os.environ['DB_PASSWORD_PG'],
         'HOST': 'switchyard.proxy.rlwy.net',
         'PORT': '47469',
 
