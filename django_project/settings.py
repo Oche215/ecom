57ddf3c215b9c328f2f54b0d7562691c0b120204
Samson OCHE <s.oche@outlook.com>
2025-05-14 16:24:11 +0100

    buynow 1

--- django_project/settings.py
+++ django_project/settings.py
@@ -170,5 +170,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #paypal settings
 PAYPAL_TEST = True
 PAYPAL_RECEIVER_EMAIL = 'merchant@lct.com' #business sandbox account
-PAYPAL_BUY_BUTTON_IMAGE = "static/assets/buynow.png"
+PAYPAL_BUY_BUTTON_IMAGE = "https://www.pngkit.com/png/detail/23-238018_paypal-checkout-png-paypal.png"
 #'static/assets/PayPal-PayNow.png'
