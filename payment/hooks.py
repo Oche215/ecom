f4bb14dc8d1d1e6e611e85d89f72f5375c233bd7
Samson OCHE <s.oche@outlook.com>
2025-05-14 22:23:36 +0100

    paypal success

--- payment/hooks.py
+++ payment/hooks.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from paypal.standard.models import ST_PP_COMPLETED
 from paypal.standard.ipn.signals import valid_ipn_received
 from django.dispatch import receiver
@@ -5,15 +6,27 @@ from django.conf import settings
 import time
 
 from payment.models import Order
+from payment.views import payment_success
 
 
 @receiver(valid_ipn_received)
-def paypay_payment(sender, **kwargs):
+def paypal_payment(sender, **kwargs):
     time.sleep(4)
     ipn_object = sender
 
+    # Extract data from ipn_object
+    transaction_id = ipn_object.txn_id
+    payment_status = ipn_object.payment_status
+    amount = ipn_object.mc_gross
+    payer_email = ipn_object.payer_email
+
     my_invoice = str(ipn_object.invoice)
     my_order = Order.objects.get(invoice=my_invoice)
+
     my_order.paid = True
     my_order.save()
+
+    return redirect('payment_success')
+
+
 
