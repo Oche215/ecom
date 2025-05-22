0784e38f6515c52482014d8627cfbe7587aeefae
Samson OCHE <s.oche@outlook.com>
2025-05-22 17:01:35 +0100

    ipn.invoice3

--- payment/models.py
+++ payment/models.py
@@ -37,7 +37,6 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     shipped = models.BooleanField(default=False)
     date_shipped = models.DateTimeField(blank=True, null=True)
-
     #PayPal Invoice and Paid T/F
     invoice = models.CharField(max_length=250, null=True, blank=True)
     paid = models.BooleanField(default=False)
